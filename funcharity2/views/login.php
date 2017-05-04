eb3a6636ca3cbf46a8d487655499278e08e71ecc
MuchammadYusuf_6706154063 <yusufsuf@gmail.com>
2017-05-05 05:31:20 +0700

    Update login 1.5

--- funcharity2/views/login.php
+++ funcharity2/views/login.php
@@ -4,8 +4,8 @@
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('file/bootstrap/css/login-style.css') ?>">
 </head>
 <body style="background-color:#546E7A">
-    <div class="login-card">
-        <h1>Login Admin</h1>
+	<div class="login-car">
+		<h1>Login Admin</h1>
         <form action="<?php echo base_url('index.php/login/aksi_login'); ?>" method="post" class = "form-horizontal">			
             <input type="text" name="username" placeholder="Username">
             <input type="password" name="password" placeholder="Password">
